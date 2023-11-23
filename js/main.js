b16e289ab27fa17aa8acd186f3279255b1ed482b
Scara-08 <kackinbaevaaana@gmail.com>
2023-11-23 15:44:23 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+
+
+    class User {
+        
+
+        constructor(name, age) {
+        this.name = 'Yelan';
+        this.age = 26;
+        }
+        
+        print() {
+        console.log("Yelan " + this.name + " 26 " + this.age);
+         }   
+        }
+
+        var user = new User("Yelan", 26 );
+        user.print();
+
+
+
+
+
